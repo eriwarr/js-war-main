060746d8ac9200ef398a3505c602bf9ff23f5155
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-05-26 16:10:57 -0400

    fully functional game

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function Card(number) {
 Deck.prototype.createDeck = function() {
   for (var i = 2; i <= 14; i++) {
     for (var x = 0; x < 4; x++) {
-      aCard = new Card(i);
+      let aCard = new Card(i);
       this.deck.push(aCard);
     }
   }
@@ -42,7 +42,7 @@ function shuffle(deck) {
 
 //Function deals half of the cards to each player//
 Deck.prototype.dealCards = function() {
-  for (i = 0; i < this.deck.length; i++) {
+  for (var i = 0; i < this.deck.length; i++) {
     if (i % 2) {
       game.player1.hand.push(this.deck[i]);
     } else {
@@ -86,7 +86,7 @@ Game.prototype.compareCard = function(){
     //Adds player2's card to the front of player1's deck//
     game.player1.hand.unshift(player2Card);
     //Removes the last card from player1's deck//
-    x = game.player1.hand.pop();
+    var x = game.player1.hand.pop();
     //Adds the last card from player1's deck to the front//
     game.player1.hand.unshift(x);
     //Removes the card removes the card from player2's deck//
@@ -94,143 +94,47 @@ Game.prototype.compareCard = function(){
   } else if (player1Card.number < player2Card.number) {
     console.log(`${player2} threw out a ${player2Card.number}. ${player1} threw out a ${player1Card.number}. ${player2} takes the cards. ${player2} has ${game.player2.hand.length} cards.`);
     game.player2.hand.unshift(player1Card);
-    x = game.player2.hand.pop();
+    var x = game.player2.hand.pop();
     game.player2.hand.unshift(x);
     game.player1.hand.pop();
 
   } else if (player1Card.number === player2Card.number) {
     console.log(`IT'S WAR BABY!`)
-    for(i = 0; i < 4; i++){
-    x = game.player1.hand.pop();
-    y = game.player2.hand.pop();
+    for(var i = 0; i < 4; i++){
+    var x = game.player1.hand.pop();
+    var y = game.player2.hand.pop();
       game.pot.push(x);
       game.pot.push(y);
 
     }
-    if(game.pot[5].number > game.pot[6].number){
+    if(game.pot[game.pot.length - 2].number > game.pot[game.pot.length - 1].number){
       console.log(`I DECLARE THAT....${player1} wins the WAR`);
-      game.player1.hand.push(game.pot[0], game.pot[1], game.pot[2], game.pot[3], game.pot[4], game.pot[5]);
-      game.pot = [];
-    } else if(game.pot[5].number < game.pot[6].number) {
-      console.log(`I DECLARE THAT....${player2} wins the WAR`);
-      game.player2.hand.push(game.pot[0], game.pot[1], game.pot[2], game.pot[3], game.pot[4], game.pot[5]);
-      game.pot = [];
-    } else if (game.pot[5].number === game.pot[6].number){
-        this.compareCard();
+      for(var i = 0; i < game.pot.length; i++){
+        game.player1.hand.push(game.pot[i]);
+      }
         game.pot = [];
+    } else if(game.pot[game.pot.length - 2].number < game.pot[game.pot.length - 1].number) {
+      console.log(`I DECLARE THAT....${player2} wins the WAR`);
+      for(var i = 0; i < game.pot.length; i++){
+        game.player2.hand.push(game.pot[i]);
+      }
+        game.pot = [];
+    } else {
+        this.compareCard();
     }
 
   }
 
 }
 
-game = new Game();
+const game = new Game();
 game.start();
 
 
 
 if (game.player1.hand.length > game.player2.hand.length){
-  alert(`${player1} wins!!!!!!`);
+  alert(`${player1} wins with ${game.player1.hand.length} cards!!!!!!`);
 }
 if (game.player1.hand.length < game.player2.hand.length){
-  alert(`${player2} wins!!!!!!`);
+  alert(`${player2} wins with ${game.player2.hand.length} cards!!!!!!`);
 }
-
-// // Mady's Solution//
-//
-// (function() {
-//   'use strict'
-//
-// const Card = function({value, suit} = {}) {
-//   this.value = value;
-//   this.suit = suit;
-// }
-//
-//
-// const Deck = function(){
-//   this.cards = [];
-//   this.suits = ['clubs', 'hearts', 'spades', 'diamonds']
-//
-//   for(let i = 0; i < 4; i++){
-//     for(let j = 2; j <= 14; j++) {
-//       this.cards.push(new Card({suit: this.suits[i], value: j}));
-//     }
-//   }
-// }
-//
-// const Player = function({name}){
-//   this.name = name;
-//   this.hand = [];
-//   this.cardCount = 0;
-//   this.draw = null;
-//
-// }
-//
-// const Game = function() {
-//   const player1 = prompt(`Enter player one's name: `);
-//   const player2 = prompt(`Enter player two's name: `);
-//   this.player1 = new Player({name: player1});
-//   this.player2 = new Player({name: player2});
-//   this.deck = new Deck();
-//   this.pot = [];
-//
-// }
-//
-// Game.prototype.shuffle = function(deck) {
-//   let i = deck.length, j, temp;
-//   while(i--){
-//     j = Math.floor(Math.random() * (i + 1));
-//     temp = deck[i];
-//     deck[i] = deck[j];
-//     deck[j] = temp;
-//   }
-// }
-//
-// Game.prototype.deal = function(){
-//   this.player1.hand = this.deck.cards.filter(function(item, index){
-//     return !(index % 2);
-//   });
-//
-//   this.player2.hand = this.deck.cards.filter(function(item, index) {
-//     return index % 2;
-//   });
-// }
-//
-// Game.prototype.draw = function () {
-//   const player1Card = this.player1.hand.shift();
-//   const player2Card = this.player2.hand.shift();
-//
-//   this.player1.cardCount -= 1;
-//   this.player2.cardCount -= 1;
-//
-//   if(!this.player1.cardCount){
-//     this.shuffle(this.player1.hand);
-//   }
-//   if(!this.player2.cardCount) {
-//     this.shuffle(this.player2.hand);
-//   }
-//
-//   this.player1.draw = player1Card;
-//   this.player2.draw = player2Card;
-//
-//   this.pot = [player1Card, player1Card, ...this.pot];
-//   console.log(`${this.player1.name} draws a ${this.deck.value}`);
-//   console.log(`${this.player2.name} draws a ${this.deck.value}`);
-// }
-//
-// Game.prototype.play = function() {
-//   this.shuffle(this.deck.cards);
-//   this.deal();
-//
-//   console.log('Lets play War!');
-//
-//
-//   game.draw();
-//   //while loop or control flow that continues drawing if the player has cards//
-// }
-//
-//
-//
-// const game = new Game();
-// game.play();
-// })();
